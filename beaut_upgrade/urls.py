31e64805eede92b288f7774aed11fe2fc841748f
sumit singh <sumit.singh@kiwitech.com>
2019-08-23 00:37:59 +0530

    Added static files and templates

--- beaut_upgrade/urls.py
+++ beaut_upgrade/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from newApp.views import index
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
+    url(r'', index),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
